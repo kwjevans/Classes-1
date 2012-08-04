51c5cd86bb30f168de3fbecc95fba50db2c6651a
Kris Evans <kwjevans@gmail.com>
2012-08-05 00:44:39 +0100

    Working version

--- Classes-1/main.cpp
+++ Classes-1/main.cpp
@@ -8,11 +8,43 @@
 
 #include <iostream>
 
+// Declaration of apartment struct
+
+struct apartment;
+// Declaration of counter object
+int i;
+
+
+// Definition of apartment struct
+struct apartment {
+    int number;
+    int numRooms;
+    // bool hasGarage;
+    
+    void newApartment(int number,int numRooms,bool hasGarage);
+    
+};
+
+// Create an array of placeToLive of type apartment (our struct)
+apartment placeToLive[3];
+
+// Constructor function for creating a new apartment structure
+void apartment::newApartment(int numberInit,int numRoomsInit,bool hasGarageInit){
+    number = numberInit;
+    numRooms = numRoomsInit;
+    //hasGarage = hasGarageInit;
+    };
+
 int main(int argc, const char * argv[])
 {
+    for (i=1;i<4;i++){
+        placeToLive[i].newApartment(i*2, i, 0);
+    }
+    
+    for (i=1;i<4;i++){
+        std::cout << i << ". Apartment number " << placeToLive[i].number << " has " << placeToLive[i].numRooms << " rooms\n";
+    }
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
 
